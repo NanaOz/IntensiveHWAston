15d9a045f2c06c30635d1194d3543d2c1d869a9c
Alina <alinavik93@gmail.com>
2025-04-28 18:39:35 +0500

    перестроение односвязного списка в дерево в HashMap

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,8 +1,33 @@
 package app;
 
+import java.lang.reflect.Field;
+import java.util.HashMap;
+
 public class Main {
     public static void main(String[] args) {
+        workOfMyArrayList();
 
+        workOfMyLinkedList();
+
+        workHashMapRebuilding();
+    }
+
+    private static void workHashMapRebuilding() {
+        HashMap<HashMapRebuild, String> map = new HashMap<>(8);
+
+        for (int i = 0; i < 15; i++) {
+            map.put(new HashMapRebuild("key" + i), "value" + i);
+
+            boolean isTree = i >= 8;
+
+            System.out.println("--- Итерация " + i + " ---");
+            System.out.println("Общий размер HashMap: " + map.size());
+            System.out.println("Размер цепочки в бакете: " + (i + 1));
+            System.out.println("Тип ноды: " + (isTree ? "TreeNode" : "Node"));
+        }
+    }
+
+    private static void workOfMyArrayList() {
         MyArrayList myArrayList = new MyArrayList();
         myArrayList.add("a");
         myArrayList.add("b");
@@ -11,8 +36,9 @@ public class Main {
 
         myArrayList.remove(1);
         myArrayList.display();
+    }
 
-
+    private static void workOfMyLinkedList() {
         MyLinkedList<String> myLinkedList = new MyLinkedList<>();
         myLinkedList.add("skjcd");
         myLinkedList.add("opiop");
