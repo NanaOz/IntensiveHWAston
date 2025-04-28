d3a92cb1fd225848fbdd2f9bf1367bfd8efe9721
Alina <alinavik93@gmail.com>
2025-04-28 15:22:42 +0500

    реализация ArrayList

--- src/app/Main.java
+++ src/app/Main.java
@@ -3,5 +3,14 @@ package app;
 public class Main {
     public static void main(String[] args) {
 
+        MyArrayList myArrayList = new MyArrayList();
+        myArrayList.add("a");
+        myArrayList.add("b");
+        myArrayList.add("c");
+        myArrayList.display();
+
+        myArrayList.remove(1);
+        myArrayList.display();
+
     }
 }
