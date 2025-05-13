b42591667a02da0e8491a1d3b23f6bda738fc04e
Alina <alinavik93@gmail.com>
2025-05-13 11:20:27 +0500

    DatabaseConnection - убрано в проперти, исправлен импорт

--- src/app/javacode/connection/DatabaseConnection.java
+++ src/app/javacode/connection/DatabaseConnection.java
@@ -1,13 +1,36 @@
 package app.javacode.connection;
-import java.sql.*;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.Properties;
 
 public class DatabaseConnection {
-    private static final String URL = "jdbc:postgresql://localhost:5432/testappaston";
-    private static final String USER = "postgres";
-    private static final String PASSWORD = "0025";
+    private static final String PROPERTIES_FILE = "db.properties";
+    private static String URL;
+    private static String USER;
+    private static String PASSWORD;
+
+    private static void readProperties() {
+        try (InputStream inputStream = DatabaseConnection.class.getClassLoader().getResourceAsStream(PROPERTIES_FILE)) {
+            Properties properties = new Properties();
+            properties.load(inputStream);
+
+            URL = properties.getProperty("db.url");
+            USER = properties.getProperty("db.user");
+            PASSWORD = properties.getProperty("db.password");
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     public static Connection getConnection() throws SQLException {
+        if (URL == null) {
+            readProperties();
+        }
         Properties props = new Properties();
         props.setProperty("user", USER);
         props.setProperty("password", PASSWORD);
